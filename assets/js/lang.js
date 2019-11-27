81428fa5ffc26233ed1791486ae1094c300b4c45
kavtara1 <40422017+kavtara1@users.noreply.github.com>
2019-11-27 16:14:22 +0400

    LangBar
    LangBar

---
+++ assets/js/lang.js
@@ -0,0 +1,14 @@
+let langbar = document.getElementById("lang-bar")
+let choosebar = document.getElementById("option-bar")
+
+
+langbar.addEventListener("click",function(){
+   if (choosebar.style.display === "block") {
+       choosebar.style.display = "none"
+       
+   }
+   else {
+    choosebar.style.display = "block"    
+   }
+   })
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,63 @@ burger.addEventListener("click",function(){
    }
 
 })
+$(document).ready(function(){
+//test for getting url value from attr
+// var img1 = $('.test').attr("data-thumbnail");
+// console.log(img1);
+
+//test for iterating over child elements
+var langArray = [];
+var htmlList = ["!111","!222","!333"]
+$('.vodiapicker option').each(function(){
+  var img = $(this).attr("data-thumbnail");
+  var text = this.innerText;
+  var value = $(this).val();
+  var item = '<li><a href="" id="link" ><img src="'+ img +'" alt="" value="'+value+'"/><span>'+ text +'</span></a></li>';
+  langArray.push(item);
+ 
+  
+})
+console.log(langArray)
+
+$('#a').html(langArray);
+
+//Set the button value to the first el of the array
+$('.btn-select').html(langArray[0]);
+$('.btn-select').attr('value', 'en');
+$(".btn-select a").contents().unwrap();
+
+//change button stuff on click
+$('#a li').click(function(){
+   var img = $(this).find('img').attr("src");
+   var value = $(this).find('img').attr('value');
+   var text = this.innerText;
+   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
+  $('.btn-select').html(item);
+  $('.btn-select').attr('value', value);
+  $(".b").toggle();
+  //console.log(value);
+});
+
+$(".btn-select").click(function(){
+        $(".b").toggle();
+    });
+
+//check local storage for the lang
+var sessionLang = localStorage.getItem('lang');
+if (sessionLang){
+  //find an item with value of sessionLang
+  var langIndex = langArray.indexOf(sessionLang);
+  $('.btn-select').html(langArray[langIndex]);
+  $('.btn-select').attr('value', sessionLang);
+} else {
+   var langIndex = langArray.indexOf('ch');
+  console.log(langIndex);
+  $('.btn-select').html(langArray[langIndex]);
+  //$('.btn-select').attr('value', 'en');
+}
+
+
+});
+
 
